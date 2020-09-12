839807059e975faac8c7fe743fa3c575f974a6d2
Geoff <geoff.register@gmail.com>
2020-09-12 19:42:50 -0400

    1:29:59

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import ToDoItem from './components/ToDoItem'
-import Footer from './components/Footer'
+import ContactCard from './components/ContactCard'
 
 function App() {
 
   return (
     <div className="todo-list">
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
+      <ContactCard 
+        contact = {
+          {
+            name: "Mr. Whiskerson",
+            imgUrl: "http://placekitten.com/300/200",
+            phone: "(828) 123-4567",
+            email: "kit@ten.com"
+          }
+        }
+      />
+      <ContactCard 
+        contact =  {
+          {
+            name: "Mrs. Clawface",
+            imgUrl: "http://placekitten.com/400/200",
+            phone: "(919) 123-4567",
+            email: "tony@ten.com"
+          }
+        }
+      />
+      <ContactCard 
+        contact =  {
+          {
+            name: "Todd",
+            imgUrl: "http://placekitten.com/400/300",
+            phone: "(828) 987-6543",
+            email: "billups@ten.com"
+          }
+        }
+      />
+      <ContactCard
+        contact =  {
+          {
+            name: "KoolAid",
+            imgUrl: "http://placekitten.com/200/100",
+            phone: "(919) 987-6543",
+            email: "bowler@ten.com"
+          }
+        }
+      />
     </div>
   )
 }
