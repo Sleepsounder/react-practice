cbeb65e52630c9885b01e060fac5dc85f0f253d9
Geoff <geoff.register@gmail.com>
2020-09-14 23:28:35 -0400

    Class vs Functional 2:07:21

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// function App() {
+// 	return (
+// 		<div>
+// 			Toops
+// 		</div>
+// 	)
+// }
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Toops point Oh!</h1>
+			</div>
+		)
+	}
+} 
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import ToDoApp from './ToDoApp'
 
-ReactDOM.render(
-  <ToDoApp />,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header 
+          id="14"
+          name={{bloons: "Farley", tines: "Oopsies"}}/>
+        <Greeting />
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.name.tines}!!!</p>
+      </header>
+    )
+  }
+}
+
+class Greeting extends React.Component {
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+
+    return (
+      <div>
+        <h1>Good {timeOfDay}, dudes!!!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
